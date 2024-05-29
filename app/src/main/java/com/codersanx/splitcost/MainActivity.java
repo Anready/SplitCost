ab6e7a2d248cdb9e8069ab39719c8503e3b5acdb
Anready <nfc939@gmail.com>
2024-05-29 23:27:48 +0300

    Initial commit

---
+++ app/src/main/java/com/codersanx/splitcost/MainActivity.java
@@ -0,0 +1,21 @@
+package com.codersanx.splitcost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.codersanx.splitcost.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
